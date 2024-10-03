2e30a7e1779c97d02ecfcf5a6527ab135dde089a
boradkrutil <krutilborad2020@gmail.com>
2024-10-03 16:08:51 +0530

    nothing change

--- app.js
+++ app.js
@@ -15,20 +15,17 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-
-app.get("/", (req, res) => {
-    res.send("Welcome to Task Management Application.....")
-})
-
-app.use("/api", router)
-
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT, () => {
     console.log("Server listening on port " + PORT);
     dbconnection()
 
 })
+app.get("/", (req, res) => {
+    res.send("Welcome to Task Management Application.....")
+})
+app.use("/api", router)
+
 app.use(routeNotFound)
 app.use(errorHandler)

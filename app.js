f739b0188e2b033de2a557dff8c966c7a0737d46
boradkrutil <krutilborad2020@gmail.com>
2024-10-03 15:44:51 +0530

    modify

--- app.js
+++ app.js
@@ -8,24 +8,27 @@ import { errorHandler, routeNotFound } from './middleware/errorMiddleware.js';
 
 
 dotenv.config();
-dbconnection()
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.use("/api",router)
+app.get("/", (req, res) => {
+    res.send("Welcome to Task Management Application.....")
+})
+
+app.use("/api", router)
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to Task Management Application.....")
-})
-app.listen(PORT,()=>{
-    console.log("Server listening on port " +PORT);
+
+app.listen(PORT, () => {
+    console.log("Server listening on port " + PORT);
+    dbconnection()
+
 })
 app.use(routeNotFound)
 app.use(errorHandler)
